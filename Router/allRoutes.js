4ed07e122eeca48a8b1d33e78dfcbaefaa94af0d
Florian <nairolf7793@yahoo.com>
2024-12-14 15:47:14 +0100

    add router / road / test ok, prepare to push on developp

---
+++ Router/allRoutes.js
@@ -0,0 +1,11 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.php"),
+    new Route("/evenement", "Evenement", "/pages/evenement.php"),
+    new Route("/contact", "Contact", "/pages/contact.php"),
+    new Route("/connexion", "Connexion", "/pages/connexion.php"),];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Esportify";
