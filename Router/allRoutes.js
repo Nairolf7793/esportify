6bbb3ce9e532a47fad2324c4b1faa1e267bd4538
Florian <nairolf7793@yahoo.com>
2024-12-15 17:19:42 +0100

    ajout de la route inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.php"),
     new Route("/evenement", "Evenement", "/pages/evenement.php"),
     new Route("/contact", "Contact", "/pages/contact.php"),
-    new Route("/connexion", "Connexion", "/pages/connexion.php"),];
+    new Route("/connexion", "Connexion", "/pages/connexion.php"),
+    new Route("/inscription", "Inscription", "/pages/inscription.php"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Esportify";
