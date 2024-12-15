c158e98e98c0d6adb6ae6ba188d72337f881ae5b
Florian <nairolf7793@yahoo.com>
2024-12-15 16:56:31 +0100

    instancier

--- index.php
+++ index.php
@@ -27,17 +27,15 @@
     </main>
 
     <div class="container">
-  <footer class="py-3 my-4">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Accueil</font></font></a></li>
-      <li class="nav-item"><a href="/a_propos" class="nav-link px-2 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">A propos</font></font></a></li>
-      <li class="nav-item"><a href="/RGPD" class="nav-link px-2 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarifs</font></font></a></li>
-    </ul>
-    <p class="text-center text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">© 2024 E-Sportify, Inc.</font></font></p>
-  </footer>
-</div>
-
-
+      <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Accueil</font></font></a></li>
+        <li class="nav-item"><a href="/a_propos" class="nav-link px-2 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">A propos</font></font></a></li>
+        <li class="nav-item"><a href="/RGPD" class="nav-link px-2 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarifs</font></font></a></li>
+      </ul>
+      <p class="text-center text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">© 2024 E-Sportify, Inc.</font></font></p>
+      </footer>
+    </div>
 
     <script src="./js/script.js"></script>
     <script type="module" src="/Router/Router.js"></script>
