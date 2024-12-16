dcd5920307608036397ecae38768a2d36ff9be83
Florian <nairolf7793@yahoo.com>
2024-12-16 10:47:28 +0100

    feat(connexion): ajout de sécurite sur la page connexion / inscription, regex pour MP, test ok

--- js/valideForm.js
+++ js/valideForm.js
@@ -15,10 +15,26 @@ function validateInscription(){
     validateRequired(inputPassword);
     validateRequired(inputEmail);
     validateRequired(inputAge);
+    const passwordOk = validatePassword(inputPassword);
+    
 }
 
 function validateRequired(input){
     if(input.value != ''){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+    }
+    else {
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+    }
+}
+
+function validatePassword(input){
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+
+    if(passwordUser.match(passwordRegex)){
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
     }
