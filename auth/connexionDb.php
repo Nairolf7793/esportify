390535713ee4319c9e12de4f16eadc077026a093
Florian <nairolf7793@yahoo.com>
2024-12-16 17:04:08 +0100

    feat(connexion):validation de la connexion user + affichage message erreur si erreur id

---
+++ auth/connexionDb.php
@@ -0,0 +1,40 @@
+<?php
+require_once "../db/DbConnexion.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(!$_POST['pseudo'] || !$_POST['password']) {
+        header('location: /connexion?error=missing_fields');
+    } else {
+
+        $pseudo = $_POST['pseudo'];
+        $password = $_POST['password'];
+        $db = DbConnection::getPdo();
+        $sql = ('SELECT * FROM user WHERE pseudo = :pseudo'); //requete stockée dans variable
+        $query = $db->prepare($sql);
+        
+        $query->bindParam(':pseudo', $pseudo);
+
+        $query->execute();
+
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+
+        if(!$user){
+            header('location: /connexion?error=invalid_user');
+            exit();
+
+        } else {
+           $passwordOk = password_verify($password, $user['password']);
+
+           if (!$passwordOk) {
+            header('location: /connexion?error=invalid_password');
+            exit();
+
+           } else {
+            $_SESSION["user"]= $user;
+            header('location: /');
+            exit();
+           }
+        }
+    }
+}
+?>
