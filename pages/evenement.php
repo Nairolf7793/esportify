9d68f64903eec24870c0597f97d9876f05e7bb99
Florian <nairolf7793@yahoo.com>
2024-12-19 15:39:59 +0100

    feat(evenement): mise en page d'une liste pour ajout event

--- pages/evenement.php
+++ pages/evenement.php
@@ -1,1 +1,23 @@
-<h1>Bienvenue à evenement</h1>
+<h1 class="text-center">Bienvenue à evenement</h1>
+<div class="container">
+<ol class="list-group list-group-numbered">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">evenement 1</div>
+      Créer par :
+    </div>
+    
+    <div class="me-4">
+        heure, date.
+    </div>
+    <div class="me-4">
+        <button>Bouton rejoindre</button>
+    </div>
+    <span class="badge text-bg-primary rounded-pill me-4">/ 14</span>
+    <div>
+        i
+    </div>
+
+  </li>
+</ol>
+</div>
