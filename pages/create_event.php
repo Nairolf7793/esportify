df6d6afe58019d0138fe52e5ba833230e4f561c9
Florian <nairolf7793@yahoo.com>
2024-12-19 17:21:38 +0100

    feat(create_event): mise en place du formulaire

--- pages/create_event.php
+++ pages/create_event.php
@@ -1,1 +1,23 @@
-<h1>Creer ton event</h1>
+<h1 class="text-center">Crée ton event</h1>
+<div class="container">
+<div class="mb-3">
+  <label for="titre" class="form-label">Titre</label>
+  <input type="text" class="form-control" id="titre">
+</div>
+<div class="mb-3">
+  <label for="description" class="form-label">description</label>
+  <textarea class="form-control" id="description" rows="3"></textarea>
+</div>
+<div class="mb-3">
+  <label for="nb_joueur" class="form-label">Nombre de joueur</label>
+  <input type="number" class="form-control" id="nb_joueur" min=2 max=14 placeholder="minimum deux joueurs et maximum 14 joueurs">
+</div>
+<div class="mb-3">
+  <label for="date_debut" class="form-label">Date du début</label>
+  <input type="date" class="form-control" id="date_debut" min=2 max=14 placeholder="minimum deux joueurs et maximum 14 joueurs">
+</div>
+<div class="mb-3">
+  <label for="date_fin" class="form-label">Date de fin</label>
+  <input type="date" class="form-control" id="date_fin" min=2 max=14 placeholder="minimum deux joueurs et maximum 14 joueurs">
+</div>
+</div>
