9edb43f6e97011c9e2896eb12c2547de33d3ff4c
Florian <nairolf7793@yahoo.com>
2024-12-20 15:49:04 +0100

    ajout de mon espace dans routes + navbar / test ok

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.php", "/js/deconnexion.js"),
     new Route("/evenement", "Evenement", "/pages/evenement.php"),
     new Route("/contact", "Contact", "/pages/contact.php"),
+    new Route("/account", "Mon espace", "/pages/account.php"),
     new Route("/connexion", "Connexion", "/pages/connexion.php" , "/js/valideForm.js" ),
     new Route("/create_event", "Création evenement", "/pages/create_event.php"),
     new Route("/validation_event", "validation event", "/pages/validation_event.php"),
