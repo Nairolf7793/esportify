f1ff585e1ac5f14bf253f8b891f071f5535a3d9f
Florian <nairolf7793@yahoo.com>
2024-12-20 19:23:46 +0100

    ajout d'un formulaire select pour modifier role joueur

--- pages/liste_joueur.php
+++ pages/liste_joueur.php
@@ -26,6 +26,17 @@ $user = $query->fetchAll(PDO::FETCH_ASSOC);
                 <td><?php echo ($user['age']); ?></p></td>
                 <td><?php echo ($user['mail']); ?></p></td>
                 <td><?php echo ($user['role']); ?></p></td>
+                <td>
+                            <!-- Formulaire pour modifier le rôle -->
+                            <form action="../back/update_role.php" method="POST" class="d-inline">
+                            <input type="hidden" name="user_id" value="<?php echo $user['id']; ?>">
+                                <select name="new_role" class="form-select form-select-sm" style="width: auto; display: inline-block;">
+                                    <option value="joueur" <?php echo $user['role'] === 'joueur' ? 'selected' : ''; ?>>Joueur</option>
+                                    <option value="organisateur" <?php echo $user['role'] === 'organisateur' ? 'selected' : ''; ?>>Organisateur</option>
+                                </select>
+                                <button type="submit" class="btn btn-sm btn-primary">Modifier</button>
+                            </form>
+                        </td>
             </tr>
         <?php } ?>
         </tbody>
