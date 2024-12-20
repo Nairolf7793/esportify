7e8a8c8741bd794c1463c780fe77d147755cc334
Florian <nairolf7793@yahoo.com>
2024-12-20 12:10:00 +0100

    ajout des elements de la bdd sur la page evenement, test ok

--- pages/evenement.php
+++ pages/evenement.php
@@ -1,3 +1,12 @@
+<?php 
+require_once "../db/DbConnexion.php";
+
+$query = DbConnection::getPdo()->query('SELECT * FROM event');
+$event = $query->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
 <section class="container evenemnt">
     <h1 class="text-center">Bienvenue à evenement</h1>
         <p>"Prêt à plonger dans l'aventure ? Rejoins notre communauté de gamers et trouve la partie qui te correspond. Des soirées thématiques aux tournois épiques, 
@@ -29,22 +38,24 @@
     </ol>
 
     <h2 id="inscription" class="text-center">Inscrit toi à l'evenement de ton choix</h2>
+    <?php foreach ($event as $valeur) { ?>
         <ol class="list-group list-group-numbered">
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 <div class="ms-2 me-auto">
-                    <div class="fw-bold">evenement 1</div>
+                    <div class="fw-bold"><?php echo $valeur ['titre'] ?></div>
                         Créer par :
                 </div>
                 <div class="me-4">
-                    heure, date.
+                    <?php echo $valeur ["date_debut"] ?>
                 </div>
                 <div class="me-4">
                     <button>Bouton S'inscrire</button>
                 </div>
                 <span class="badge text-bg-primary rounded-pill me-4">/ 14</span>
                 <div>
-                    i
+                    <i class="fa-solid fa-circle-info" data-bs-toogle="tooltip" data-bs-title="c'est un test" ></i>
                 </div>
             </li>
-        </ol>
+        </ol> 
+        <?php } ?>
 </section>
