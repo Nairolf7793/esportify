6b0019954dbfbee8dbcf16692c2d6406b3b8ae3c
Florian <nairolf7793@yahoo.com>
2024-12-20 18:43:23 +0100

    feat(liste_joueurs:mise en place d'un table pour listing tous les joueurs

--- pages/liste_joueur.php
+++ pages/liste_joueur.php
@@ -1,1 +1,33 @@
-<h2>Liste joueur</h2>
+<?php 
+require_once '../db/DbConnexion.php';
+require_once '../db/session.php';
+
+$pdo = DbConnection::getPdo();
+$query = $pdo->query('SELECT pseudo,age,mail,role FROM user');
+$user = $query->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<div class="container">
+    <h2>Liste joueur</h2>
+    <table class="table text-start">
+        <thead>
+            <tr>
+                <th scope="col text-start">Pseudo</th>
+                <th scope="col text-start">Age</th>
+                <th scope="col text-start">Mail</th>
+                <th scope="col text-start">Role</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($user as $user){ ?>
+            <tr>
+                <td><?php echo ($user['pseudo']); ?></td>
+                <td><?php echo ($user['age']); ?></p></td>
+                <td><?php echo ($user['mail']); ?></p></td>
+                <td><?php echo ($user['role']); ?></p></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</div>
