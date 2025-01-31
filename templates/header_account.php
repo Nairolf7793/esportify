369f9a04616e29094c896c298bcffce8e554c303
Florian <nairolf7793@yahoo.com>
2025-01-31 17:42:15 +0100

    feat(account): layout

--- templates/header_account.php
+++ templates/header_account.php
@@ -1,67 +1,29 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi pe-none me-2" width="40" height="32">
-            <use xlink:href="#bootstrap"></use>
-        </svg>
-        <span class="fs-4">Menu mon espace</span>
+        <h2 class="fs-4">Menu mon espace</h2>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-            <a href="#" class="nav-link active" aria-current="page">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#home"></use>
-                </svg>
-                Mes events
-            </a>
+            <a href="#" class="nav-link">Mes events</a>
         </li>
         <li>
-            <a href="#" class="nav-link link-body-emphasis">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#speedometer2"></use>
-                </svg>
-                Créer un événement
-            </a>
+            <a href="../create_event.php" class="nav-link">Créer un événement</a>
         </li>
         <li>
-            <a href="#" class="nav-link link-body-emphasis">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#table"></use>
-                </svg>
-                Valider les événements
-            </a>
+            <a href="#" class="nav-link">Valider les événements</a>
         </li>
         <li>
-            <a href="#" class="nav-link link-body-emphasis">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#grid"></use>
-                </svg>
-                Modifier un compte utilisateurs
-            </a>
+            <a href="#" class="nav-link">Modifier un compte utilisateurs</a>
         </li>
         <li>
-            <a href="#" class="nav-link link-body-emphasis">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#people-circle"></use>
-                </svg>
-                Historique des événements
-            </a>
+            <a href="#" class="nav-link">Historique des événements</a>
         </li>
         <li>
-            <a href="#" class="nav-link link-body-emphasis">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#grid"></use>
-                </svg>
-                Voir mes scores
-            </a>
+            <a href="#" class="nav-link">Voir mes scores</a>
         </li>
         <li>
-            <a href="#" class="nav-link link-body-emphasis">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#grid"></use>
-                </svg>
-                Modifier mon compte
-            </a>
+            <a href="#" class="nav-link">Modifier mon compte</a>
         </li>
     </ul>
 </div>
