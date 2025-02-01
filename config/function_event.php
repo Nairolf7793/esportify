080436eac1591678b9e218283a10529259f1843e
Florian <nairolf7793@yahoo.com>
2025-02-01 12:21:02 +0100

    ajout à la function event de id joueur + recuperer au formulaire pour bdd

--- config/function_event.php
+++ config/function_event.php
@@ -1,11 +1,12 @@
 <?php 
 require_once "pdo.php";
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 function addEvents (PDO $pdo, string $titre, string $description, int $nb_joueur, string $date_debut, string $heure_debut, string $date_fin, string $heure_fin){
-    $query = $pdo->prepare("INSERT INTO event (titre, description, nb_joueur, date_debut, heure_debut, date_fin, heure_fin)
-                            VALUES (:titre, :description, :nb_joueur, :date_debut, :heure_debut, :date_fin, :heure_fin)");
-
+    $id_joueur = $_SESSION['user']['id'];
+    $query = $pdo->prepare("INSERT INTO event (titre, description, nb_joueur, date_debut, heure_debut, date_fin, heure_fin, id_joueur)
+                            VALUES (:titre, :description, :nb_joueur, :date_debut, :heure_debut, :date_fin, :heure_fin, :id_joueur)");
     $query -> bindParam(":titre", $titre, PDO::PARAM_STR);
     $query -> bindParam(":description", $description, PDO::PARAM_STR);
     $query -> bindParam(":nb_joueur", $nb_joueur, PDO::PARAM_INT);
@@ -13,6 +14,7 @@ function addEvents (PDO $pdo, string $titre, string $description, int $nb_joueur
     $query -> bindParam(":heure_debut", $heure_debut, PDO::PARAM_STR);
     $query -> bindParam(":date_fin", $date_fin, PDO::PARAM_STR);
     $query -> bindParam(":heure_fin", $heure_fin, PDO::PARAM_STR);
+    $query -> bindParam(":id_joueur", $id_joueur, PDO::PARAM_INT);
 
     return $query->execute();
 }
